d26984b75130db956cdd7e30f92dcf7b9fdafeef
agreafel <csliw30@gmail.com>
2021-02-03 17:34:02 -0800

    Packages and Demo Level Started

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    private CharacterController mover;
+    [SerializeField] private float speed = 4;
+
+    private void Start()
+    {
+        mover = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction.Normalize();
+        if (direction.magnitude > 0)
+        {
+            transform.eulerAngles = Quaternion.LookRotation(direction, Vector3.up).eulerAngles;
+            mover.SimpleMove(speed * direction);
+        }
+    }
+}
